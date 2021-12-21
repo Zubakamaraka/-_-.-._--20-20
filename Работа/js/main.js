e20c56ec3e231a4a1fa036ae4c7bbb5ad9bbff7d
Zubakamaraka <48492860+Zubakamaraka@users.noreply.github.com>
2021-12-21 07:05:11 +0300

    Delete Работа directory

--- Работа/js/main.js
+++
@@ -1,7 +0,0 @@
-function Top()
-{
-window.scrollTo(pageXOffset, 0);
-};
-window.addEventListener('scroll', function() {
-arrowTop.visibale = (pageYOffset < document.documentElement.clientHeight);
-});
